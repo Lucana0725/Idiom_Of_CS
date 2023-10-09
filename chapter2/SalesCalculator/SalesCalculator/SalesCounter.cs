7ae5dc61e2557df1bfe4cd18a3172a9ae1ebeb65
Lucana <k.ark.scarlet72@gmail.com>
2023-10-09 18:45:16 +0900

    [Finish]SalesCounterクラスに店舗別に売上を求めるメソッドを定義

--- chapter2/SalesCalculator/SalesCalculator/SalesCounter.cs
+++ chapter2/SalesCalculator/SalesCalculator/SalesCounter.cs
@@ -17,5 +17,24 @@ namespace SalesCalculator
         {
             _sales = sales;
         }
+
+        // 店舗別売上を求めるメソッド
+        public Dictionary<string, int> GetPerStoreSales()
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            foreach (Sale sale in _sales)   // Saleオブジェクトを1行ずつ受け取って処理したいので、"Sale sales in _sales"
+            {
+                if (dict.ContainsKey(sale.ShopName))    // Dictionaryのキーとして、指定した(回ってきたSaleの1行に)店舗名が含まれていれば
+                {
+                    dict[sale.ShopName] += sale.Amount;     // 同じ店舗として売上を加算する
+                } 
+                else
+                {
+                    dict[sale.ShopName] = sale.Amount;      // 違う店舗なので、売上は加算せずそのまま
+                }
+            }
+            return dict;
+        }
+
     }
 }
