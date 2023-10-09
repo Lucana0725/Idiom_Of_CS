2d8f17d4adad5b6ea79ec83aa548ba311e1833aa
Lucana <k.ark.scarlet72@gmail.com>
2023-10-09 18:58:05 +0900

    [Finish]ProgramクラスのMainメソッドに集計結果を出力する処理を記述

--- chapter2/SalesCalculator/SalesCalculator/Program.cs
+++ chapter2/SalesCalculator/SalesCalculator/Program.cs
@@ -11,6 +11,18 @@ namespace SalesCalculator
     {
         static void Main(string[] args)
         {
+            /*
+             * SalesCounter sales = new SalesCounter(ReadSales("sales.csv")); に関して
+             * SalesCounterインスタンスを生成するときにsales.csvを渡したReadSales()を渡し、ReadSales()がsales.csvを読み込んでSaleオブジェクトのリストを作り、
+             * SaleオブジェクトのリストがSalesCounterのコンストラクタに入りフィールド変数_saleとして格納される。そしてそれらの結果がsalesとしてここで保持される という流れ。
+             */
+            SalesCounter sales = new SalesCounter(ReadSales("sales.csv"));
+
+            Dictionary<string, int> amountPerStore = sales.GetPerStoreSales();
+            foreach (KeyValuePair<string, int> obj in amountPerStore)
+            {
+                Console.WriteLine($"{obj.Key}：{obj.Value}円");
+            }
         }
 
         // 売上データを読み込み、Saleオブジェクトのリストを返す
