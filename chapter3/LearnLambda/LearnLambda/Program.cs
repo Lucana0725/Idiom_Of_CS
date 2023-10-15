cd4d29a2fcf44a6ff09529d3f6f345c4db8f942e
Lucana <k.ark.scarlet72@gmail.com>
2023-10-15 21:55:41 +0900

    ラムダ式の学習

---
+++ chapter3/LearnLambda/LearnLambda/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnLambda {
+	internal static class Program {
+		static void Main(string[] args) {
+			var word = "gateman";
+			var result = word.Reverse();
+			Console.WriteLine(result);
+			
+		}
+		
+		public static string Reverse(this string str) {
+			if (string.IsNullOrWhiteSpace(str)) { return string.Empty; }
+			char[] chars = str.ToCharArray();
+			Array.Reverse(chars);
+			return new string(chars);
+		}
+	}
+}
